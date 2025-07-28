4d5a5b46fb9cd780d04c3ea760f0856a147d5415
Mudkipman98 <mudkipman98@gmail.com>
2025-07-28 16:44:36 -0700

    *sigh* fixed the todo so it's actually representative of what I need to do...

--- stats.py
+++ stats.py
@@ -4,7 +4,7 @@ def word_count(transcript):
 
 characters_in_book = {'t': 0, 'h': 0, 'e': 0, ' ': 0, 'p': 0, 'r': 0, 'o': 0, 'j': 0, 'c': 0, 'g': 0, 'u': 0, 'n': 0, 'b': 0, 'k': 0, 'f': 0, 'a': 0, 's': 0, 'i': 0, ';': 0, ',': 0, 'm': 0, 'd': 0, '\n': 0, 'y': 0, 'w': 0, 'l': 0, 'v': 0, '.': 0, '-': 0, ':': 0, '2': 0, '3': 0, '0': 0, '1': 0, '[': 0, '#': 0, '4': 0, '5': 0, ']': 0, '&': 0, '8': 0, '/': 0, '*': 0, '’': 0, 'x': 0, '_': 0, 'q': 0, '?': 0, '—': 0, '6': 0, 'z': 0, '(': 0, ')': 0, '7': 0, 'æ': 0, '!': 0, '“': 0, '”': 0, '9': 0, 'ë': 0, '‘': 0, 'â': 0, 'ê': 0, 'ô': 0, '™': 0, '•': 0, '%': 0, '$': 0}
 
-#TODO: TURN THE DICT INTO A LIST OF DICTS, EACH ONE CONTAINING A KEY-VALUE PAIR WHERE THE KEY IS A VALUE, IE "'6':0," BECOMES {"char": "key", "num": value}
+#TODO: TURN THE DICT INTO A LIST OF DICTS, EACH ONE CONTAINING A KEY-VALUE PAIR WHERE THE KEY IS A VALUE, IE "'6':0," BECOMES {"char": "6", "num": 0}
 
 def char_count_in_book(book_text):
     for i in book_text:
