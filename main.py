b2ac910ee529e9c9a8564b8c3416b04531d2213e
Mudkipman98 <mudkipman98@gmail.com>
2025-07-28 10:40:53 -0700

    Added .gitignore, slight working modifcations to main.py.

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ def get_book_text():
         book_text = f.read()
         return book_text
 
+def words_in_string(words_in_book):
+    
+    
+
 def main():
-    print(get_book_text())    
+    words_in_book = get_book_text    
 
 main()
 
