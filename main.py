4e5e0e54a27ad09aed56bb10b47c336f693b800b
Mudkipman98 <mudkipman98@gmail.com>
2025-07-28 16:32:25 -0700

    Segmented code by moving the word_count function to stats.py.  added a dictionary of all characters in the book, to lowercase.  Added the ability to count each character and print a dict with each count to the console.

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+from stats import word_count, char_count_in_book
+
+
+
 def get_book_text():
     with open("books/frankenstein.txt") as f:
 
         book_text = f.read()
         return book_text
 
-def word_count(transcript):
-    word_count = len(transcript.split())
-    return word_count
+
     
 
 def main():
-    book_text = get_book_text() #this gets the entire transcript of the book, works
+    book_text = get_book_text() 
+    book_text = book_text.lower() #all of the book's text in lowercase
     
     num_words = word_count(book_text) 
     print (f"{num_words} words found in the document")
 
+    char_count = char_count_in_book(book_text)
+
+    print(char_count)
 main()
 
