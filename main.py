32ff34db1bbebf3c156ca2e6b777ee3c27fbab86
Mudkipman98 <mudkipman98@gmail.com>
2026-01-01 16:18:46 -0800

    Cleaning up the code to be more readable, including comments to document my process as I return to this project.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, char_count_in_book, all_chars_in_book
+from stats import word_count, char_count_in_book, all_chars_in_book, sorted_dict
 
 
 
@@ -8,30 +8,33 @@ def get_book_text():
         book_text = f.read()
         return book_text
 
-
-
-
 def main():
-    book_text = get_book_text()
-    book_text = book_text.lower() #all of the book's text in lowercase
+    book_text = get_book_text() #assign the text of our document to the variable 'book_text'
+    book_text = book_text.lower() #update book_text to be in all lowercase
 
-    num_words = word_count(book_text)
-    print (f"{num_words} words found in the document")
+    num_words = word_count(book_text) #count the words in book_text
+    print (f"{num_words} words found in the document") #print the wordcount
 
     char_count = char_count_in_book(book_text)
 
-    print(char_count)
+    #print(char_count) #prints the updated characters_in_book found in stats.py.  It's an unsorted dictionary at this point, with key:value pairs of {'character': 'occurrence', 'character': 'occurrence', etc.}
 
-    sorted_chars_in_book = all_chars_in_book(char_count)
+    list_of_dicts_of_chars = all_chars_in_book(char_count)
 
-    for i in sorted_chars_in_book:
+    for i in list_of_dicts_of_chars:
         key = i.keys()
         key_list = list(key)
 
         if key_list[0].isalpha() != True:
-            sorted_chars_in_book = sorted_chars_in_book.pop(sorted_chars_in_book[i])
+            list_of_dicts_of_chars = list_of_dicts_of_chars.pop(list_of_dicts_of_chars[i])
 
 
-    print(sorted_chars_in_book)
+    print(list_of_dicts_of_chars) #This gives us the dictionary we need to sort, then make pretty later.
+
+    sorted_characters_by_occurance = sorted_dict(list_of_dicts_of_chars)
+
+    print(sorted_characters_by_occurance)
+    
+
 main()
 
